bc6f1353198010b3252baaef6aa06b5452e80e5d
Pduhard <pacodj@free.fr>
2021-11-11 20:44:43 +0100

    some architecture idea

---
+++ GomokuLib/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='GomokuLib',
+    version='0.1.0',
+    author='Pduhard - Agiordan',
+    description='',
+    keywords='lib',
+    packages=[
+        'GomokuLib',
+    ],
+    # long_description=open('README.md').read(),
+    install_requires=[
+        # 'numpy==1.19.3',
+        # 'numba==0.43.1'
+    ]
+)
